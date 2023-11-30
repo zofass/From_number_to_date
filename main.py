a3fb98110facdf95f8151fdd090ec35800f5901a
zofass <chachachka@gmail.com>
2023-11-30 02:39:43 +0200

    Ukr version

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-user_input = int(input("Please enter number of seconds (from 0 to 8640000): "))
+user_input = int(input("Будь ласка, введіть число від 0 до 8640000: "))
 def format_time(seconds):
     if 0 <= seconds < 8640000:
         days, remainder = divmod(seconds, 24 * 60 * 60)
         hours, remainder = divmod(remainder, 60 * 60)
         minutes, seconds = divmod(remainder, 60)
 
-        days_word = "day" if days == 1 else "days"
+        days_word = "день" if days == 1 else "дні" if 2 <= days <= 4 else "днів"
         hours_str = str(hours).zfill(2)
         minutes_str = str(minutes).zfill(2)
         seconds_str = str(seconds).zfill(2)
@@ -13,7 +13,7 @@ def format_time(seconds):
         formatted_time = f"{days} {days_word}, {hours_str}:{minutes_str}:{seconds_str}"
         return formatted_time
     else:
-        return "Invalid value. Enter a number between 0 and 8639999"
+        return "Невірне значення. Введіть числоу проміжку між 0 та 8639999"
 
 result_time = format_time(user_input)
 print(result_time)
